25539c0d34b5591f9f3032d96b3e3c49226de45d
venkat yashwanth <mudiliyashwanth@gmail.com>
2024-01-22 22:58:35 +0530

    added Super Over League

---
+++ SuperOverLeague/script.js
@@ -0,0 +1,11 @@
+const element = (
+    <div className="super-over-league-container">
+        <h1 className="heading">Super Over League</h1>
+        <div className="teams-container">
+            <img src="./assets/images/rcb-img.png" alt="rcb" className="logo-image"/>
+            <img src="./assets/images/csk-img.png" alt="csk" className="logo-image"/>
+        </div>
+    </div>
+)
+
+ReactDOM.render(element, document.getElementById("root"))
